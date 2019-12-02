eeda67a8434f21f50c3b90a21e8eb424e261dd5e
Miff <miff@miffthefox.info>
2019-12-02 07:06:52 -0500

    Add Angle type

---
+++ MiffTheFox.Geometry.Tests/AngleTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiffTheFox.Geometry.Tests
+{
+    [TestClass]
+    public class AngleTests
+    {
+        const double DELTA = 1e-8; // good ol' floating-points
+        
+        [TestMethod]
+        [DataRow(0.5, AngleUnit.Turns, 0.5)]
+        // TODO: test other units
+        public void TestCreationFromUnits(double value, AngleUnit unit, double expected)
+        {
+            var angle = new Angle(value, unit);
+            Assert.AreEqual(expected, angle.Turns);
+        }
+
+        [TestMethod]
+        [DataRow(0.5, Math.PI)]
+        [DataRow(0.75, Math.PI * 1.5)]
+        // TODO: more test cases
+        public void TestRadiansProperty(double turns, double expectedRadians)
+        {
+            var angle = new Angle(turns);
+            Assert.AreEqual(expectedRadians, angle.Radians);
+        }
+
+        [TestMethod]
+        [DataRow(0.0, AngleUnit.Degrees, 0.0)]
+        [DataRow(0.25, AngleUnit.Degrees, 90.0)]
+        [DataRow(0.5, AngleUnit.Degrees, 180.0)]
+        [DataRow(1.0, AngleUnit.Degrees, 360.0)]
+        // TODO: test cases for each other unit
+        public void TestToUnit(double turns, AngleUnit toUnit, double expected)
+        {
+            var angle = new Angle(turns);
+            Assert.AreEqual(expected, angle.ToUnit(toUnit));
+        }
+    }
+}
---
+++ MiffTheFox.Geometry/Angle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiffTheFox.Geometry
+{
+    public readonly struct Angle
+    {
+        public double Turns { get; }
+        public double Radians => ToUnit(AngleUnit.Radians);
+
+        public Angle(double value, AngleUnit unit)
+            => Turns = value / _GetConstantForAngleUnit(unit);
+
+        public Angle(double turns)
+            => Turns = turns;
+
+        public Angle(Portion turns)
+            => Turns = turns.Value;
+
+        public double ToUnit(AngleUnit unit) => Turns * _GetConstantForAngleUnit(unit);
+        
+        private static double _ConvertUnit(double value, AngleUnit from, AngleUnit to)
+        {
+            double factor = _GetConstantForAngleUnit(to) / _GetConstantForAngleUnit(from);
+            return value * factor;
+        }
+
+        private static double _GetConstantForAngleUnit(AngleUnit unit)
+        {
+            const double tau = Math.PI * 2;
+            switch (unit)
+            {
+                case AngleUnit.Turns: return 1;
+                case AngleUnit.Degrees: return 360;
+                case AngleUnit.Radians: return tau;
+                case AngleUnit.PiRadians: return 2;
+                case AngleUnit.Gradians: return 400;
+                case AngleUnit.Percent: return 100;
+                default: throw new ArgumentException("Unknown angle unit.", nameof(unit));
+            }
+        }
+    }
+
+    public enum AngleUnit : byte
+    {
+        /// <summary>Turns, where a complete circle is 1. Also the unit in radians as a multiple of τ (2π).</summary>
+        Turns = 0,
+        /// <summary>Degrees, where a complete circle is 360°.</summary>
+        Degrees = 1,
+        /// <summary>Radians, where a complete circle is 2π.</summary>
+        Radians = 2,
+        /// <summary>Radians as a multiple of π, where a complete circle is 2.</summary>
+        PiRadians = 3,
+        /// <summary>Gradians, where a complete circle is 400.</summary>
+        Gradians = 4,
+        /// <summary>Percent, where a complete circle is 100.</summary>
+        Percent = 5
+    }
+}
